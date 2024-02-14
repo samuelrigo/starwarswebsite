e6e4fc3b02b90bb8fe88ecdba8ff3dfd0c52e7d4
samel <samuelrigo@gmail.com>
2024-02-14 14:42:02 -0300

    Added Header to Router for better practice and removed index from header

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -22,3 +22,4 @@ export const Header = () => {
     </header>
   );
 };
+export default Header;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/home.page";
 import Movies from "../pages/Movies/movies.page";
+import Header from "../components/header/header.component";
 
 export const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies/" element={<Movies />} />
