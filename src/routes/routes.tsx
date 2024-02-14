53d2927b4efc2e71dcb069bd949e64a618888b0b
samel <samuelrigo@gmail.com>
2024-02-14 18:31:24 -0300

    removed movies old page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/home.page";
-import Movies from "../pages/Movies/movies.page";
 import Header from "../components/header/header.component";
 
 export const Router = () => {
@@ -9,7 +8,6 @@ export const Router = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies/" element={<Movies />} />
       </Routes>
     </BrowserRouter>
   );
