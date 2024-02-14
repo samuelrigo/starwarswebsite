5408a19d648693166231be2fa641369e297c594e
samel <samuelrigo@gmail.com>
2024-02-14 09:34:38 -0300

    Removed route "Home"

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "../pages/Home/home.page"
-import Movies from "../pages/Movies/movies.page"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/home.page";
+import Movies from "../pages/Movies/movies.page";
 
-export const Router = () => { 
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element= {<Home />} />
-                <Route path="/home" element= {<Home />} />
-                <Route path="/movies/" element={<Movies />} />
-            </Routes>
-        </BrowserRouter>
-  )
-}
-export default Router
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies/" element={<Movies />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;
