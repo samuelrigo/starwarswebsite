4cdc27e0fad637bf48696026f6e4f0e6962256e9
samel <samuelrigo@gmail.com>
2024-02-14 18:31:09 -0300

    added new section on the front page

--- src/pages/Home/home.page.tsx
+++ src/pages/Home/home.page.tsx
@@ -1,6 +1,14 @@
+import { CharactersSection } from "../../components/home/characters/characters.section.component";
 import { FrontPage } from "../../components/home/frontpage/frontpage.component";
+import { MoviesSection } from "../../components/home/movies/movies.section.component";
 
 export const Home = () => {
-  return <FrontPage />;
+  return (
+    <div>
+      <FrontPage />
+      <CharactersSection />
+      <MoviesSection />
+    </div>
+  );
 };
 export default Home;
