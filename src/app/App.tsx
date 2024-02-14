cd9e6be788e4602c05a979344444ef6d14836ee8
samel <samuelrigo@gmail.com>
2024-02-14 14:40:30 -0300

    Made a global stylesheet for better practices

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,11 @@
 import Router from "../routes/routes";
+import "./global.styles.css";
 
 export const App = () => {
-    return (
-        <Router />
-    );
-}
-export default App
+  return (
+    <main className="main-container">
+      <Router />
+    </main>
+  );
+};
+export default App;
