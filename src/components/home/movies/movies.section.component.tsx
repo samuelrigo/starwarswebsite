8913d8387beb34e128a7447b66173ef41cea791f
samel <samuelrigo@gmail.com>
2024-02-19 18:52:03 -0300

    made a specific section for movies

--- src/components/home/movies/movies.section.component.tsx
+++ src/components/home/movies/movies.section.component.tsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 import "./movies.section.styles.css";
+import useMovies from "../../movies/body/use-movies.hook";
+
 export const MoviesSection = () => {
+  const { movies, hasMovies } = useMovies();
   return (
     <div className="movies">
-      <h1> Movies </h1>
-      <Link to="/movies" className="movies__seeAll">
-        See All
-      </Link>
+      <div className="movies__nav">
+        <h1> Movies </h1>
+        <Link to="/movies" className="movies__seeAll">
+          See All
+        </Link>
+      </div>
+
+      <ul>
+        {hasMovies &&
+          movies.map(({ title, director, release_date, characters }) => {
+            return (
+              <li key={title}>
+                <strong>{title}</strong>
+                <p>{director}</p>
+                <p>{release_date}</p>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 };
