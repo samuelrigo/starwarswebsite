75c7201fff077102763bb5cd28c690e38d5ed142
samel <samuelrigo@gmail.com>
2024-02-28 16:15:49 -0300

    Add movie cards for first three movies

--- src/components/movies/body/movies.component.tsx
+++ src/components/movies/body/movies.component.tsx
@@ -4,6 +4,7 @@ import useMovies from "./use-movies.hook";
 
 export const MoviesPage = (props: any) => {
   const { movies, hasMovies } = useMovies();
+  const firstThreeMovies = movies.slice(0, 3);
   return (
     <div className="movies">
       {props.seeAll !== undefined ? (
@@ -21,16 +22,62 @@ export const MoviesPage = (props: any) => {
 
       <ul className="movie__list">
         {hasMovies &&
-          movies.map(({ title, director, release_date, characters }) => {
-            return (
-              <div className="movie__card" key={title}>
-                <strong>{title}</strong>
-                <p>
-                  {director} - {release_date}
-                </p>
-              </div>
-            );
-          })}
+          firstThreeMovies.map(
+            ({ title, director, release_date, characters }) => {
+              switch (title) {
+                case "A New Hope":
+                  return (
+                    <div className="movie__card" key={title}>
+                      <img
+                        src="./././images/movies/newhope.png"
+                        alt="A New Hope"
+                        className="movie__img"
+                      />
+                      <div className="movie__info">
+                        <h1>{title}</h1>
+                        <p>
+                          {director} - {release_date}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                case "The Empire Strikes Back":
+                  return (
+                    <div className="movie__card" key={title}>
+                      <img
+                        src="./././images/movies/theempirestrikesback.png"
+                        alt="The Empire Strikes Back"
+                        className="movie__img"
+                      />
+                      <div className="movie__info">
+                        <h1>{title}</h1>
+                        <p>
+                          {director} - {release_date}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                case "Return of the Jedi":
+                  return (
+                    <div className="movie__card" key={title}>
+                      <img
+                        src="./././images/movies/returnofjedi.png"
+                        alt="Return of the Jedi"
+                        className="movie__img"
+                      />
+                      <div className="movie__info">
+                        <h1>{title}</h1>
+                        <p>
+                          {director} - {release_date}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                default:
+                  return null;
+              }
+            }
+          )}
       </ul>
     </div>
   );
