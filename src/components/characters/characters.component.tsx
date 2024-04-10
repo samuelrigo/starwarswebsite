e9fdacaef4b3ef80caff6f1db668f11d84a7c839
samel <samuelrigo@gmail.com>
2024-04-10 18:03:31 -0300

    Refactor header and characters component styles and add media queries for responsiveness

--- src/components/characters/characters.component.tsx
+++ src/components/characters/characters.component.tsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
 import "./characters.styles.css";
 import useCharacters from "./use-characters.hook";
+import { useRef, useState } from "react";
 
 export const CharactersPage = (props: any) => {
   const { characters, hasCharacters } = useCharacters();
+  const charRef = useRef<any>();
+  const [visibleChar, setVisibleChar] = useState<string | null>(null);
+
+  const showCharInfo = (name: string) => {
+    setVisibleChar(name);
+  };
+
+  const hideCharInfo = () => {
+    setVisibleChar(null);
+  };
 
   return (
     <div className="characters__page">
@@ -21,123 +32,250 @@ export const CharactersPage = (props: any) => {
           <h1 className="characters__title"> Characters </h1>
         </div>
       )}
-    {props.seeAll ? (
-      <ul className="card__list">
-        {hasCharacters &&
-          characters.map(function ({
-            name,
-            films,
-            species,
-            mass,
-            height,
-            birth_year,
-            homeworld,
-          }) {
-            switch (name) {
-              case "Luke Skywalker":
-                return (
-                  <div
-                    className="
-                   card__luke"
-                  >
-                    <img
-                      src="./././images/luke_charcard_mobile.png"
-                      alt="Luke Skywalker"
-                      className="card__luke__img"
-                    />
-                    <div className="card__info__luke " key={name + Math.random}>
-                      <h1>
-                        <strong> {name}</strong>
-                      </h1>
-                      <p>Movies:</p>
-                      <strong> {films.join(", ")}</strong>
-
-                      <p>Species:</p>
-                      <strong>{species}</strong>
-
-                      <p>Mass:</p>
-                      <strong> {mass} Kg</strong>
-
-                      <p>Height:</p>
-                      <strong> {height} m</strong>
-
-                      <p>Birth Year: </p>
-                      <strong> {birth_year}</strong>
-
-                      <p>Homeworld:</p>
-                      <strong> {homeworld}</strong>
-                    </div>
-                  </div>
-                );
-              case "Darth Vader":
-                return (
-                  <div className="card__darth">
-                    <img
-                      src="./././images/darth_charcard_mobile.png"
-                      alt="Darth Vader"
-                      className="card__darth__img"
-                    />
-                    <div className="card__info__darth" key={name + Math.random}>
-                      <h1>
-                        <strong> {name}</strong>
-                      </h1>
-                    </div>
-                  </div>
-                );
-              case "Yoda":
-                return (
-                  <div className=" card__yoda">
-                    <img
-                      src="./././images/yoda_charcard_mobile.png"
-                      alt="Yoda"
-                      className="card__yoda__img"
-                    />
-                    <div className="card__info__yoda" key={name + Math.random}>
-                      <h1>
-                        <strong> {name}</strong>
-                      </h1>
-                    </div>
-                  </div>
-                );
-              case "Anakin Skywalker":
-                return (
-                  <div className="card__anakin">
-                    <img
-                      src="./././images/anakin_charcard_mobile.png"
-                      alt="Anakin Skywalker"
-                      className="card__anakin__img"
-                    />
-                    <div
-                      className="card__info__anakin"
-                      key={name + Math.random}
-                    >
-                      <h1>
-                        <strong> {name}</strong>
-                      </h1>
-                    </div>
-                  </div>
-                );
-              default:
-                return null;
-            }
-          })}
-      </ul>
-      ):(
+      {props.seeAll ? (
         <ul className="card__list">
-        {hasCharacters &&
-          characters.map(function ({
-            name,
-          }) {
-            return (
-              <div className="card" key={name + Math.random}>
-                <h1 className="card__characters__title">
-                  <strong> {name}</strong>
-                </h1>
-              </div>
-            );
-          })}
-          </ul>
-      )};
+          {hasCharacters &&
+            characters.map(function ({
+              name,
+              films,
+              species,
+              mass,
+              height,
+              birth_year,
+              homeworld,
+            }) {
+              switch (name) {
+                case "Luke Skywalker":
+                  return (
+                    <div
+                      className="
+                   card__luke"
+                    >
+                      <img
+                        src="./././images/mobile/luke_charcard_mobile.png"
+                        alt="Luke Skywalker"
+                        className="card__luke__img__mobile"
+                      />
+                      <img
+                        src="./././images/tablet/luke_charcard_tablet.png"
+                        alt="Luke Skywalker"
+                        className="card__luke__img__tablet"
+                      />
+                      <img
+                        src="./././images/desktop/luke_charcard_desktop.png"
+                        alt="Luke Skywalker"
+                        className="card__luke__img__desktop"
+                      />
+                      <div
+                        className="card__info__luke "
+                        key={name + Math.random}
+                      >
+                        <div className="card__info__title__luke">
+                          <h1>
+                            <strong> {name}</strong>
+                          </h1>
+                        </div>
+
+                        <div className="card__info__movies__luke">
+                          <p>Movies:</p>
+                          <strong> {films.join(", ")}</strong>
+                        </div>
+                        <div className="card__info__details__luke">
+                          <div className="card__info__secundary__luke">
+                            <p>Species:</p>
+                            <strong>{species}</strong>
+                          </div>
+                          <div className="card__info__secundary__luke">
+                            <p>Mass:</p>
+                            <strong> {mass} Kg</strong>
+                          </div>
+                          <div className="card__info__secundary__luke">
+                            <p>Height:</p>
+                            <strong> {height} m</strong>
+                          </div>
+                          <div className="card__info__secundary__luke">
+                            <p>Birth Year: </p>
+                            <strong> {birth_year}</strong>
+                          </div>
+                          <div className="card__info__secundary__luke">
+                            <p>Homeworld:</p>
+                            <strong> {homeworld}</strong>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                case "Darth Vader":
+                  return (
+                    <div className="card__darth">
+                      <img
+                        src="./././images/mobile/darth_charcard_mobile.png"
+                        alt="Darth Vader"
+                        className="card__darth__img__mobile"
+                      />
+                      <img
+                        src="./././images/tablet/darth_charcard_tablet.png"
+                        alt="Darth Vader"
+                        className="card__darth__img__tablet"
+                      />
+                      <img
+                        src="./././images/desktop/darth_charcard_desktop.png"
+                        alt="Darth Vader"
+                        className="card__darth__img__desktop"
+                      />
+                      <div
+                        className="card__info__darth"
+                        key={name + Math.random}
+                      >
+                        <h1>
+                          <strong> {name}</strong>
+                        </h1>
+                      </div>
+                    </div>
+                  );
+                case "Yoda":
+                  return (
+                    <div className=" card__yoda">
+                      <img
+                        src="./././images/mobile/yoda_charcard_mobile.png"
+                        alt="Yoda"
+                        className="card__yoda__img__mobile"
+                      />
+                      <img
+                        src="./././images/tablet/yoda_charcard_tablet.png"
+                        alt="Yoda"
+                        className="card__yoda__img__tablet"
+                      />
+                      <img
+                        src="./././images/desktop/yoda_charcard_desktop.png"
+                        alt="Yoda"
+                        className="card__yoda__img__desktop"
+                      />
+                      <div
+                        className="card__info__yoda"
+                        key={name + Math.random}
+                      >
+                        <h1>
+                          <strong> {name}</strong>
+                        </h1>
+                      </div>
+                    </div>
+                  );
+                case "Anakin Skywalker":
+                  return (
+                    <div className="card__anakin">
+                      <img
+                        src="./././images/mobile/anakin_charcard_mobile.png"
+                        alt="Anakin Skywalker"
+                        className="card__anakin__img__mobile"
+                      />
+                      <img
+                        src="./././images/tablet/anakin_charcard_tablet.png"
+                        alt="Anakin Skywalker"
+                        className="card__anakin__img__tablet"
+                      />
+                      <img
+                        src="./././images/desktop/anakin_charcard_desktop.png"
+                        alt="Anakin Skywalker"
+                        className="card__anakin__img__desktop"
+                      />
+
+                      <div
+                        className="card__info__anakin"
+                        key={name + Math.random}
+                      >
+                        <h1>
+                          <strong> {name}</strong>
+                        </h1>
+                      </div>
+                    </div>
+                  );
+                default:
+                  return null;
+              }
+            })}
+        </ul>
+      ) : (
+        <ul className="card__list">
+          {hasCharacters &&
+            characters.map(function ({
+              name,
+              films,
+              species,
+              mass,
+              height,
+              birth_year,
+              homeworld,
+            }) {
+              return (
+                <div className="card" key={name + Math.random}>
+                  <div className="card__characters__title">
+                    <h1>
+                      <strong> {name}</strong>
+                    </h1>
+                    {visibleChar !== name && (
+                      <button
+                        className="card__character__button"
+                        onClick={() => showCharInfo(name)}
+                      >
+                        <img
+                          src="./././images/arrow_down_icon.png"
+                          alt="Arrow down button"
+                        ></img>
+                      </button>
+                    )}
+                    {visibleChar === name && (
+                      <button
+                        className="card__character__button"
+                        onClick={hideCharInfo}
+                      >
+                        <img
+                          src="./././images/arrow_up_icon.png"
+                          alt="Arrow up button"
+                        ></img>
+                      </button>
+                    )}
+                  </div>
+
+                  {visibleChar === name && (
+                    <div className="hidden__char__info">
+                      {
+                        <div className="shown__char__info">
+                          <div className="card__info__movies">
+                            <p>Movies:</p>
+                            <strong> {films.join(", ")}</strong>
+                          </div>
+                          <div className="card__info__details">
+                            <div className="card__info__secundary">
+                              <p>Species:</p>
+                              <strong>{species}</strong>
+                            </div>
+                            <div className="card__info__secundary">
+                              <p>Mass:</p>
+                              <strong> {mass} Kg</strong>
+                            </div>
+                            <div className="card__info__secundary">
+                              <p>Height:</p>
+                              <strong> {height} m</strong>
+                            </div>
+                            <div className="card__info__secundary">
+                              <p>Birth Year: </p>
+                              <strong> {birth_year}</strong>
+                            </div>
+                            <div className="card__info__secundary">
+                              <p>Homeworld:</p>
+                              <strong> {homeworld}</strong>
+                            </div>
+                          </div>
+                        </div>
+                      }
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+        </ul>
+      )}
     </div>
   );
 };
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -3,70 +3,70 @@ import { Link } from "react-router-dom";
 import "./header.styles.css";
 
 export const Header = () => {
-    const [scrolled, setScrolled] = useState(false);
-    const navRef = useRef<any>();
+  const [scrolled, setScrolled] = useState(false);
+  const navRef = useRef<any>();
 
-    const showNavBar = () => {
-        navRef.current.classList.toggle("responsive__nav");
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive__nav");
+  };
+
+  const hideNavbar = () => {
+    navRef.current.classList.remove("responsive__nav");
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
     };
 
-    const hideNavbar = () => {
-        navRef.current.classList.remove("responsive__nav");
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      // cleanup
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const isScrolled = window.scrollY > 0;
-            setScrolled(isScrolled);
-        };
+  return (
+    <header className={`header ${scrolled ? "scrolled" : ""}`}>
+      <button className="nav__btn" onClick={showNavBar}>
+        <img src="./././images/icon-button.svg" alt="Menu button" />
+      </button>
 
-        window.addEventListener('scroll', handleScroll);
+      <Link to="/">
+        <img
+          src="./././images/star_wars_logo.svg"
+          alt="Star Wars Logo"
+          className="header__logo"
+        />
+      </Link>
 
-        return () => {
-            // cleanup
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+      <img
+        src="./././images/x__icon.svg"
+        alt="X button"
+        className="invisible__button"
+      />
 
-    return (
-        <header className={`header ${scrolled ? 'scrolled' : ''}`}>
-            <button className="nav__btn" onClick={showNavBar}>
-                <img src="./././images/icon-button.svg" alt="Menu button" />
-            </button>
+      <nav className="nav__menu" ref={navRef}>
+        <Link to="/characters" onClick={hideNavbar} className="nav__item">
+          <img
+            src="./././images/characters__icon.svg"
+            alt="Characters Icon"
+          ></img>
+          <p>Characters</p>
+        </Link>
 
-            <Link to="/">
-                <img
-                    src="./././images/star_wars_logo.svg"
-                    alt="Star Wars Logo"
-                    className="header__logo"
-                />
-            </Link>
-
-            <img
-                src="./././images/x__icon.svg"
-                alt="X button"
-                className="invisible__button"
-            />
-
-            <nav className="nav__menu" ref={navRef}>
-                <Link to="/characters" onClick={hideNavbar} className="nav__item">
-                    <img
-                        src="./././images/characters__icon.svg"
-                        alt="Characters Icon"
-                    ></img>
-                    <p>Characters</p>
-                </Link>
-
-                <Link to="/movies" onClick={hideNavbar} className="nav__item">
-                    <img src="./././images/movies__icon.svg" alt="Movies Icon"></img>
-                    <p>Movies</p>
-                </Link>
-                <button className="nav__btn nav__close__btn" onClick={showNavBar}>
-                    <img src="./././images/x__icon.svg" alt="X button" />
-                </button>
-            </nav>
-        </header>
-    );
+        <Link to="/movies" onClick={hideNavbar} className="nav__item">
+          <img src="./././images/movies__icon.svg" alt="Movies Icon"></img>
+          <p>Movies</p>
+        </Link>
+        <button className="nav__btn nav__close__btn" onClick={showNavBar}>
+          <img src="./././images/x__icon.svg" alt="X button" />
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
